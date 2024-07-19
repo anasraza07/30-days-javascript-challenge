5bd3af0f8910fc7b86c966135f35ffb3fb586f3a
anasraza07 <ar470684@gmail.com>
2024-07-19 22:45:49 +0500

    day 02 completed

---
+++ day-02/Operators.js
@@ -0,0 +1,72 @@
+// task 01
+let add = 23 + 23;
+console.log(add);
+
+// task 02
+let subtract = 32 - 12;
+console.log(subtract);
+
+// task 03
+const multiply = 23 * 23;
+console.log(multiply);
+
+// task 04
+const divide = 22 / 2;
+console.log(divide);
+
+// task 05
+const remainder = 23 % 5;
+console.log(remainder);
+
+// task 06
+add += 12;
+console.log(add)
+
+// task 07
+subtract -= 6;
+console.log(subtract);
+
+// task 08
+console.log('task 08');
+const comparisonOperator = 12 > 23;
+console.log(comparisonOperator);
+
+const comparisonOperatorTwo = 21 < 43;
+console.log(comparisonOperatorTwo);
+
+// task 09
+console.log('task 09');
+const comparisonOperatorThree = 34 <= 23;
+console.log(comparisonOperatorThree);
+
+const comparisonOperatorFour = 41 >= 41;
+console.log(comparisonOperatorFour);
+
+// task 10
+console.log('task 10');
+const comparisonOperatorFive = 40 == 41;
+console.log(comparisonOperatorFive);
+
+const comparisonOperatorSix = "41" === "41";
+console.log(comparisonOperatorSix);
+
+// task 11
+console.log('Logical Operators');
+const logicalOperatorOne = 10 < 20 && 10 > 20;
+console.log(logicalOperatorOne);
+
+// task 12
+const logicalOperatorTwo = 10 < 20 || 10 > 20;
+console.log(logicalOperatorTwo);
+
+// task 13
+const logicalOperatorThree = !"negate a string which is true";
+console.log(logicalOperatorThree);
+
+// task 14 
+const number = -12;
+number < 0 ? console.log('Number is negative') : console.log('Number is positive');
+
+
+
+
